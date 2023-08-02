d72cf26830828790b07806f9f0edb56767497b37
deepakrajkranti <draj_be19@thapar.edu>
2023-08-03 01:19:49 +0530

    sidebar made responsive some dashboard item design

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -3,7 +3,19 @@
 const DashboardPage =()=> {
   return (
     <div>
-    <p>dasboard page</p>
+    <div className="mb-8 space-y-4">
+      <h2 className="text-2xl md:text-4l font-bold text-center">
+        Explore the power of AI
+      </h2>
+    <p className="text-center text-muted-foreground
+    text-sm md:text-lg
+    ">
+      Chat with the Smartest AI.
+    </p>
+    </div>
+    <div className="px-4 md:px-20 lg:px-32  space-y-4">
+
+    </div>
     </div>
   );
 }
